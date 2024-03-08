4bb4c9fd7a3b4bf950120b019a81fba51cbde30d
marcorivas06 <MRIVASR@emory.edu>
2024-03-08 02:38:23 -0500

    added backend end

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,44 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import { Footer, Navbar } from "./components";
 import { About, Contact, Home, Projects } from "./pages";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  
+  useEffect(() => {
+    const fetchIp = async () => {
+      try {
+        const response = await fetch("https://api.ipify.org?format=json");
+        const data = await response.json();      
+        fetch("https://portfoliomanager.herokuapp.com/api/ipaddresses", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ip:data.ip}),
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchIp();
+  }, []);
+
   return (
-    <main className='bg-slate-300/20'>
+    <main className="bg-slate-300/20">
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<About />} />
+          <Route path="/" element={<About />} />
           <Route
-            path='/*'
+            path="/*"
             element={
               <>
                 <Routes>
-                  <Route path='/about' element={<About />} />
-                  <Route path='/projects' element={<Projects />} />
-                  <Route path='/contact' element={<Contact />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/projects" element={<Projects />} />
+                  <Route path="/contact" element={<Contact />} />
                 </Routes>
                 <Footer />
               </>
